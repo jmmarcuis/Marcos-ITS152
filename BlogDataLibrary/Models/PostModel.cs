3170a6d0e6e5051800298305de5a50dd98abc231
jmmarcuis <marcosjhainnoallrick@gmail.com>
2024-06-12 01:29:56 +0800

    :3

---
+++ BlogDataLibrary/Models/PostModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataLibrary.Models
+{
+    public class PostModel
+    {
+        public int ID { get; set; }
+
+        public string UserID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string DateCreated{ get; set; }
+    }
+}
